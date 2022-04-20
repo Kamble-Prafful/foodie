fb2b3a5c4e3093ec3c6be96864ba32141da34f07
Mysterious <73134414+cloutop@users.noreply.github.com>
2022-04-20 20:18:34 +0530

    Responsivev

--- script.js
+++ script.js
@@ -40,5 +40,6 @@ menuTabs.addEventListener("click", function(e) {
         const menuSection = document.querySelector(".menu-section");
         menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
         menuSection.querySelector(target).classList.add("active");
+        AOS.init();
     }
 })
